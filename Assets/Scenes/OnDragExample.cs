7367000ee96d69debb4c64b2f13beae4e06d75c0
Lovchikov Roman <a-s_roman@mail.ru>
2019-06-03 17:09:26 +0300

    1

---
+++ Assets/Scenes/OnDragExample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnDragExample : MonoBehaviour {
+    void Start() {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.Drag;
+        entry.callback.AddListener((data) => { OnDragDelegate((PointerEventData)data); });
+        trigger.triggers.Add(entry);
+    }
+
+    public void OnDragDelegate(PointerEventData data) {
+        Debug.Log("Dragging. " + data.delta);
+        transform.localPosition += new Vector3(data.delta.x, data.delta.y, 0);
+    }
+}
---
+++ Assets/Scenes/UILineConnector.cs
@@ -0,0 +1,115 @@
+﻿/// Credit Alastair Aitchison
+/// Sourced from - https://bitbucket.org/UnityUIExtensions/unity-ui-extensions/issues/123/uilinerenderer-issues-with-specifying
+
+namespace UnityEngine.UI.Extensions
+{
+    [AddComponentMenu("UI/Extensions/UI Line Connector")]
+    [RequireComponent(typeof(UILineRenderer))]
+    [ExecuteInEditMode]
+    public class UILineConnector : MonoBehaviour
+    {
+
+        // The elements between which line segments should be drawn
+        public RectTransform[] transformsFromTo;
+        [SerializeField] private Vector3[] positionsBezier;
+        [SerializeField] private Vector2[] previousPositions;
+        [SerializeField] private RectTransform canvas;
+        [SerializeField] private RectTransform rt;
+        [SerializeField] private UILineRenderer lr;
+
+        public Vector3[] canvasSpaces;
+
+        private Quaternion prevRotation;
+
+        private void Awake()
+        {
+
+            canvas = GetComponentInParent<RectTransform>().GetParentCanvas().GetComponent<RectTransform>();
+            rt = GetComponent<RectTransform>();
+            lr = GetComponent<UILineRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (transformsFromTo == null || transformsFromTo.Length < 1)
+            {
+                return;
+            }
+            //Performance check to only redraw when the child transforms move 
+            //or if this transform rotation changed
+            if (previousPositions != null && previousPositions.Length == transformsFromTo.Length
+                && prevRotation == transform.rotation)
+            {
+                bool updateLine = false;
+                for (int i = 0; i < transformsFromTo.Length; i++)
+                {
+                    if (!updateLine && previousPositions[i] != transformsFromTo[i].anchoredPosition)
+                    {
+                        updateLine = true;
+                    }
+                }
+                if (!updateLine) return;
+            }
+            prevRotation = transform.rotation;
+
+            // Get the pivot points
+            Vector2 thisPivot = rt.pivot;
+            Vector2 canvasPivot = canvas.pivot;
+
+
+
+            // Set up some arrays of coordinates in various reference systems
+            positionsBezier = new Vector3[4];
+
+            Vector3[] worldSpaces = new Vector3[transformsFromTo.Length];
+             canvasSpaces = new Vector3[transformsFromTo.Length];
+            Vector2[] points = new Vector2[positionsBezier.Length];
+
+            // First, convert the pivot to worldspace
+            for (int i = 0; i < transformsFromTo.Length; i++)
+            {
+                worldSpaces[i] = transformsFromTo[i].TransformPoint(thisPivot);
+            }
+
+            // Then, convert to canvas space
+            for (int i = 0; i < transformsFromTo.Length; i++)
+            {
+                canvasSpaces[i] = canvas.InverseTransformPoint(worldSpaces[i]);
+            }
+
+            // Add bezier points
+            positionsBezier = new Vector3[4];
+            positionsBezier[0] = canvasSpaces[0]; //start
+            positionsBezier[3] = canvasSpaces[1]; //end 
+
+            positionsBezier[1] = new Vector3(transformsFromTo[1].anchoredPosition.x, transformsFromTo[0].anchoredPosition.y, 0);
+            positionsBezier[2] = new Vector3(transformsFromTo[0].anchoredPosition.x, transformsFromTo[1].anchoredPosition.y, 0);
+
+
+
+            // Calculate delta from the canvas pivot point
+            for (int i = 0; i < positionsBezier.Length; i++)
+            {
+                points[i] = new Vector2(positionsBezier[i].x, positionsBezier[i].y);
+            }
+
+            // And assign the converted points to the line renderer
+            lr.Points = points;
+            lr.RelativeSize = false;
+            lr.drivenExternally = true;
+
+            previousPositions = new Vector2[transformsFromTo.Length];
+            for (int i = 0; i < transformsFromTo.Length; i++)
+            {
+                previousPositions[i] = transformsFromTo[i].anchoredPosition;
+            }
+        }
+
+        void AddPointsForBezier() {
+            
+
+            
+        }
+    }
+}
